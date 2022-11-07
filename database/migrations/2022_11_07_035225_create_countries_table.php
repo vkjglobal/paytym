1dcf4d72b61448b19eefb2b1ef6f991a377ac9eb
noufal <noufal.protracked@gmail.com>
2022-11-07 09:28:23 +0530

    Admin Assets

---
+++ database/migrations/2022_11_07_035225_create_countries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('currency')->nullable();
+            $table->string('tax')->nullable();
+            $table->string('fnpf')->nullable();
+            $table->boolean('status')->default(1)->comment('0-InActive, 1-Active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+};
