7b843d9047e137add093408b61b6c1e7f082cf5b
arjun <arjunlal.reubro@gmail.com>
2023-01-31 17:57:19 +0530

    31-01-23_Dashboard_bug fixes

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Branch;
+use App\Models\Department;
 
 class Project extends Model
 {
     use HasFactory;
+    
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
