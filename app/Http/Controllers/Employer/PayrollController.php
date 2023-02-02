b58df1324afd2c412af9bea04ccda484ac7b9954
= <aswinjithp7@gmail.com>
2023-02-02 09:14:18 +0530

    Payroll task 02-02-23

---
+++ app/Http/Controllers/Employer/PayrollController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Employer;
+
+use App\Http\Controllers\Controller;
+use App\Models\Payroll;
+use Illuminate\Http\Request;
+
+class PayrollController extends Controller
+{
+    public function index(){
+        $payrolls = Payroll::all();
+
+        return view('employer.payroll.index', compact('payrolls'));
+    }
+}
