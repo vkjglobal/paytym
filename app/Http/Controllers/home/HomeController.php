d890104ab51efff406cc5d421f41ca3808df9918
neena <neena.reubro@gmail.com>
2023-02-03 22:55:26 +0530

    Landing page-Pricing 03-02-2023

--- app/Http/Controllers/home/HomeController.php
+++ app/Http/Controllers/home/HomeController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\home;
 
 use App\Http\Controllers\Controller;
+use App\Models\Subscription;
+use App\Models\Cms;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function show(){
-        return view('home.index');
+        $subscription = Subscription::get();
+        $cms = Cms::get();
+       // return view('home.index','subscription');
+        return view('home.index', compact('subscription','cms'));
     }
 }
