25f134a5f290781f2acd39d467fc248b4c4a06d4
arjun <arjunlal.reubro@gmail.com>
2023-03-28 10:50:05 +0530

    Paytym_28-03-23_api_updation

--- app/Models/Payroll.php
+++ app/Models/Payroll.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Payroll extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->payslip_number = Str::uuid()->toString();
+        });
+    }
 
     public function user(){
         return $this->belongsTo(User::class);
