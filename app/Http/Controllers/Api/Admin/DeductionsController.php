7e36c136dc0fb780700807602d33389910986b84
= <aswinjithp7@gmail.com>
2023-03-30 18:08:43 +0530

    paytym 6:15

--- app/Http/Controllers/Api/Admin/DeductionsController.php
+++ app/Http/Controllers/Api/Admin/DeductionsController.php
@@ -32,7 +32,7 @@ class DeductionsController extends Controller
         // $deduction = AssignDeduction::with(['employee:id,first_name,last_name,branch_id','employee.branch:id,name','deduction:id,name,description'])->where('employer_id', $employer_id)->get();
         // $total_deduction =  AssignDeduction::;
         $deduction = User::select(['id', 'first_name', 'last_name', 'branch_id','department_id'])->with('assign_deduction.deduction:id,name,description')
-                    ->where('employer_id', $employer_id)->get();
+                    ->where('employer_id', $employer_id)->has('assign_deduction')->get();
         // $employees = User::where('employer_id', $employer_id)->get();
         // foreach($deduction as $employee){
         //     $total_deduction = AssignDeduction::where('user_id', $employee->id)->sum('rate');
