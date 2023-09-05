602465a0879c5fc856f5e4340f68814e55b64d75
neena <neena.reubro@gmail.com>
2023-09-05 16:14:41 +0530

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('invoices:add')
-        ->monthlyOn(05, '16:11')
+        ->monthlyOn(05, '16:17')
         ->timezone('Asia/Kolkata'); 
         $schedule->command('db:backup')->daily();
         $schedule->command('send:employment-over-email')->daily();
